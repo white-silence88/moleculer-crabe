a3ab29e39c1b170fdda88d03c99bfbd97a04f18a
Dmitry Shevelev <igrave1988@gmail.com>
2018-01-16 21:51:10 +0500

    Update application.pgsql.adapter.js

--- configs/application.pgsql.adapter.js
+++ configs/application.pgsql.adapter.js
@@ -2,7 +2,7 @@
 const AdapterSettings = {
     database: process.env.PGSQL_DBNAME || 'crabe',
     username: process.env.PGSQL_USERNAME || 'postgres',
-    password: process.env.PGSQL_PWD || '351skl9vcr',
+    password: process.env.PGSQL_PWD || '123456789',
     postgres: {
         host: process.env.PGSQL_HOST || 'localhost',
         port: process.env.PGSQL_PORT || 5432,
